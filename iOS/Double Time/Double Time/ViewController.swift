c1f6acda6b730a83dd110937a8681f9a1d868c20
jake <jacob.bijani@gmail.com>
2016-02-23 22:51:22 -0500

    first pass at a global timer / updating screen with time

--- iOS/Double Time/Double Time/ViewController.swift
+++ iOS/Double Time/Double Time/ViewController.swift
@@ -6,20 +6,49 @@
 //  Copyright © 2016 Totally Viable. All rights reserved.
 //
 
+import Foundation
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // MARK: Properties
+    @IBOutlet weak var barAProgressLabel: UILabel!
+    @IBOutlet weak var barBProgressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        startGlobalTimer()
     }
+    
+    var globalTimer = NSTimer()
+    var globalTimerSecondsElapsed = 0
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    var timerATime = 5
+    var timerBTime = 3
+    
+    func startGlobalTimer() {
+        globalTimer = NSTimer.scheduledTimerWithTimeInterval(1.0, target: self, selector: "globalTimerCountUp", userInfo: nil, repeats: true)
     }
-
-
+    
+    func globalTimerCountUp() {
+        globalTimerSecondsElapsed += 1
+        
+        if (globalTimerSecondsElapsed == 5) {
+//            globalTimerSecondsElapsed = 0
+        }
+        
+        updateText()
+    }
+    
+    func secondsToHoursMinutesSeconds (seconds : Int) -> (String, String, String) {
+        return (String(format: "%02d", seconds / 3600), String(format: "%02d", (seconds % 3600) / 60), String(format: "%02d", seconds % 60))
+    }
+    
+    func updateText() {
+        let (h,m,s) = secondsToHoursMinutesSeconds(globalTimerSecondsElapsed)
+        
+        barAProgressLabel.text = "\(h):\(m):\(s)"
+    }
 }
 
